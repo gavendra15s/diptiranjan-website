2136f62367cfa43641916e9993a85eab0c46770d
gavendra15s <gavendrasingh@ece.du.ac.in>
2026-05-22 20:09:07 +0530

    Add files via upload
    Initial website upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Research from './pages/Research';
+import Publications from './pages/Publications';
+import Teaching from './pages/Teaching';
+import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
+import './App.css';
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/research" element={<Research />} />
+          <Route path="/publications" element={<Publications />} />
+          <Route path="/teaching" element={<Teaching />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
